8f564ead829fb2723e4f52cc3e65acb2e0ec2ccc
Leoš Hampl <xhampl2@fi.muni.cz>
2025-04-24 19:19:02 +0200

    feat: add date converter
    Check if the changed date is correct.

---
+++ BookManagerApp/Converters/DateConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BookManagerApp.Converters
+{
+    public class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) {
+                return null;
+            }
+            if (value.ToString() == "") {
+                return null;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime result;
+            if (DateTime.TryParse(value.ToString(), out result)) {
+                return result;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
