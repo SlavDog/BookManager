8536f5d2279e4d21c82b932156330cbd5cc3ebb1
Leoš Hampl <xhampl2@fi.muni.cz>
2025-04-24 19:16:54 +0200

    feat: add Graph View functionality

---
+++ BookManagerApp/Converters/RatingConverter.cs
@@ -0,0 +1,42 @@
+﻿using BookManagerApp.DataAccessLayer;
+using BookManagerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BookManagerApp.Converters
+{
+    public class RatingConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+            if (int.TryParse(value.ToString(), out int rating))
+            {
+                return Math.Clamp(rating, 0, 5);
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string str = value as string;
+
+            if (string.IsNullOrWhiteSpace(str))
+                return null;
+
+            if (int.TryParse(str, out int rating))
+                return rating;
+
+            return null;
+        }
+    }
+}
---
+++ BookManagerApp/Views/Graph.xaml.cs
@@ -0,0 +1,31 @@
+﻿using BookManagerApp.DataAccessLayer;
+using BookManagerApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookManagerApp.Views
+{
+    /// <summary>
+    /// Interakční logika pro Graph.xaml
+    /// </summary>
+    public partial class Graph : Window
+    {
+        public Graph(ICollection<Book> books)
+        {
+            InitializeComponent();
+            var graphViewModel = new GraphViewModel(books);
+            DataContext = graphViewModel;
+        }
+    }
+}
