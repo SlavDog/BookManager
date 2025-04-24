b754bfa434698417fdf1877cffc45683c4a08a44
Leoš Hampl <xhampl2@fi.muni.cz>
2025-04-24 19:21:47 +0200

    feat: add Unknown Genre instead of null

--- BookManagerApp/Models/ComboBoxValues.cs
+++ BookManagerApp/Models/ComboBoxValues.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace BookManagerApp.Models
     public static class ComboBoxValues
     {
         public static string[] Genres { get; set; } = [
-            "Action and Adventure", "Biography and Memoir",
+            "Unknown", "Action and Adventure", "Biography and Memoir",
             "Children’s", "Classics", "Contemporary Fiction", "Crime and Mystery",
             "Dystopian", "Fantasy", "Graphic Novels", "Historical Fiction", "Horror",
             "Literary Fiction", "Non-Fiction", "Philosophy", "Poetry", "Romance",
@@ -17,7 +18,7 @@ namespace BookManagerApp.Models
         ];
 
         public static string[] Bookshelves { get; set; } = [
-            "Read", "Currently Reading", "Want To Read", "Abandoned"
+            "Completed", "Currently Reading", "Want To Read", "Abandoned"
         ];
     }
 }
