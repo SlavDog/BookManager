f16df2198efd911e31bd98b29e2d396d525ab9a3
Leoš Hampl <xhampl2@fi.muni.cz>
2025-04-27 00:39:42 +0200

    feat: finish csv import and export

---
+++ BookManagerApp/Csv/BookExportDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManagerApp.Csv
+{
+    public class BookExportDTO
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int? MyRating { get; set; }
+        public DateTime? FinishDate { get; set; }
+        public string Bookshelf { get; set; }
+        public string Genre { get; set; }
+    }
+}
---
+++ BookManagerApp/Csv/BookMap.cs
@@ -0,0 +1,17 @@
+﻿using CsvHelper.Configuration;
+
+namespace BookManagerApp.Csv
+{
+    public class BookMap : ClassMap<BookExportDTO>
+    {
+        public BookMap()
+        {
+            Map(b => b.Title).Name("Title");
+            Map(b => b.Author).Name("Author");
+            Map(b => b.MyRating).Name("My Rating");
+            Map(b => b.FinishDate).Name("Date Read");
+            Map(b => b.Bookshelf).Name("Exclusive Shelf");
+            Map(b => b.Genre).Name("Genre").Optional();
+        }
+    }
+}
