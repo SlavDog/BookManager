7b3294ad564d43044ec336321e3bb26a1a69048d
Leoš Hampl <xhampl2@fi.muni.cz>
2025-04-27 00:40:33 +0200

    refactor: move csv functions to separate file

--- BookManagerApp/Models/BookManager.cs
+++ BookManagerApp/Models/BookManager.cs
@@ -7,7 +7,6 @@ namespace BookManagerApp.Models
 {
     public class BookManager
     {
-        static int counter = 0;
         public async static Task<bool> AddNewBook(Book book, User user)
         {
             using var context = new AppDbContext();
@@ -89,95 +88,5 @@ namespace BookManagerApp.Models
                             && (b.Genre == null ? false : b.Genre.Contains(genreFilter)));
             return newBooks;
         }
-
-        public async static Task ImportBooksFromCSV(string filePath, User user)
-        {
-            using var context = new AppDbContext();
-            using (var sourceStream =
-            new FileStream(
-                filePath,
-                FileMode.Open, FileAccess.Read, FileShare.Read,
-                bufferSize: 4096, useAsync: true))
-            {
-                using (var reader = new StreamReader(sourceStream))
-                {
-                    using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
-                    {
-                        Delimiter = ",",
-                        Quote = '"'
-                    }))
-                    {
-                        await foreach (var record in csv.GetRecordsAsync<dynamic>())
-                        {
-                            var dict = (IDictionary<string, object>)record;
-
-                            int rating;
-                            Int32.TryParse(dict["My Rating"]?.ToString(), out rating);
-
-                            DateTime dateTime;
-                            DateTime.TryParse(dict["Date Read"]?.ToString(), out dateTime);
-
-                            if (await context.Books
-                                .FirstOrDefaultAsync(b => b.Title == dict["Title"].ToString() 
-                                                          && b.Username == user.Username
-                                                          && b.Author == dict["Author"].ToString()) != null) 
-                            {
-                                continue;
-                            }
-                            var book = new Book(dict["Author"].ToString() ?? $"Author{counter}",
-                                                    dict["Title"].ToString() ?? $"Book{counter++}",
-                                                    convertGoodReadsBookshelf(dict["Exclusive Shelf"]?.ToString() ?? "to-read"),
-                                                    user.Username,
-                                                    dict["My Rating"].ToString() == "" ? null : rating,
-                                                    dict["Date Read"].ToString() == "" ? null : dateTime,
-                                                    "Unknown");
-                            user.Books.Add(book);
-                            context.Add(book);
-                        }
-                    }
-                }
-                await context.SaveChangesAsync();
-            }
-        }
-
-        public static async Task ExportBooksToCSV(string filePath, User user)
-        {
-            using var context = new AppDbContext();
-            using (var sourceStream =
-            new FileStream(
-                filePath,
-                FileMode.Create, FileAccess.Write, FileShare.None,
-                bufferSize: 4096, useAsync: true))
-            {
-                using (var writer = new StreamWriter(sourceStream))
-                {
-                    using (var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)
-                    {
-                        Delimiter = ",",
-                        Quote = '"'
-                    }))
-                    {
-                        var infoToExport = user.Books
-                            .Select(b => new {
-                                b.Title,
-                                b.Author,
-                                b.MyRating,
-                                b.FinishDate,
-                                b.Bookshelf,
-                                b.Genre
-                            });
-                        await csv.WriteRecordsAsync(infoToExport);
-                    }
-                }
-            }
-        }
-
-        private static Func<string, string> convertGoodReadsBookshelf = input => input switch
-        {
-            "read" => "Completed",
-            "currently-reading" => "Currently Reading",
-            "to-read" => "Want To Read",
-            _ => "Want To Read"
-        };
     }
 }
